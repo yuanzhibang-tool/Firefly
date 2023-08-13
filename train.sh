dcdd27752fd8eda857e36efac79c232f440b17af
shandawei <shandawei@me.com>
2023-08-13 23:32:18 +0800

    更新训练参数

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
-export TRANSFORMERS_CACHE=/mnt/blockstorage/.cache
+export TRANSFORMERS_CACHE=/root/autodl-fs/.cache
+export WANDB_PROJECT=baichuan-7b-sft-qlora
 torchrun --nproc_per_node=1 train_qlora.py --train_args_file train_args/qlora/baichuan-7b-sft-qlora.json
